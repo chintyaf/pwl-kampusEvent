e88119a7cacdf2a0730884a1bc7c31f16da67845
chintyaf <chintya.elysia@gmail.com>
2025-05-27 13:53:18 +0700

    Update setup buat back side

---
+++ frontend/app/Http/Controllers/ComiteController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ComiteController extends Controller
+{
+    public function index()
+    {
+        return view(
+            'comite.index',[]
+        );
+    }
+
+    public function add()
+    {
+        return view('program-studi.add');
+    }
+
+    public function store(Request $request)
+    {
+        return redirect(route('program_studi.index', absolute: false))
+        ->with('status', 'Program Studi berhasil ditambahkan');
+    }
+
+
+
+    public function edit($id)
+    {
+        return view('program-studi.edit', []);
+    }
+
+    public function update(Request $request, $id)
+    {
+    }
+
+    public function delete($id)
+    {
+    }
+}
---
+++ frontend/resources/views/layouts/partials-front/footer.blade.php
@@ -0,0 +1,7 @@
+  <footer>
+    <div class="container">
+      <div class="col-lg-12">
+        <p>Copyright © 2025 Evoria. All rights reserved.</p>
+      </div>
+    </div>
+  </footer>
