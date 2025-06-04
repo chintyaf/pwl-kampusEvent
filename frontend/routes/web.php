deeab5b953c11e3c7d0eb9d5c85e758298a46a2c
chintyaf <chintya.elysia@gmail.com>
2025-06-04 09:23:31 +0700

    Update web.php

--- frontend/routes/web.php
+++ frontend/routes/web.php
@@ -13,26 +13,7 @@ Route::controller(HomeController::class)->group(function () {
     Route::get('event/{id}', 'view')->name('event.view');
 });
 
-Route::controller(EventRegistrationController::class)->group(function(){
-    Route::get('event/{id}/register', 'register')->name('event.register');
-
-});
-
-Route::get('/events/detail', function () {
-    return view('events.detail');
-});
-
-Route::get('/event1', function () {
-    return view('event-register.detail');
-});
-
-Route::get('/event1/register', function () {
-    return view('event-register.register');
-});
-
-Route::get('/event1/payment', function () {
-    return view('event-register.payment');
-});
+Route::get('event/{id}/register', [EventRegistrationController::class, 'register'])->name('event.register');
 
 Route::get('/event1/registered', function () {
     return view('event-register.registered');
@@ -62,13 +43,32 @@ Route::get('/admin/manage-users', function () {
     return view('admin.manage-users');
 });
 
-// Route::get('/committee', function () {
-//     return view('committee.index');
-// });
-
 Route::get('/admin/users/member', function () {
     return view('admin.list-member');
 })->name('admin.list-member');
+
+
+// COMITE
+Route::prefix('committee')->prefix('committee')->group(function () {
+    Route::controller(EventController::class)->group(function () {
+    });
+
+    Route::get('', function () {
+        return view('comite.index');
+    });
+
+
+    Route::controller(EventController::class)
+        ->prefix('events')
+        ->group(function () {
+            Route::get('', 'index')->name('event.index');
+            Route::get('add', 'add')->name('event.add');
+            Route::post('store', 'store')->name('events.store');
+            Route::get('edit/{id}', 'edit')->name('events.edit');
+            // Route::put('update/{id}', 'update')->name('events.update');
+            Route::get('delete/{id}', 'delete')->name('events.delete');
+        });
+});
 
 // FINANCE
 Route::prefix('finance')->group(function () {
@@ -93,28 +93,14 @@ Route::get('/finance/update-status', function () {
     return view('finance.update-status');
 });
 
-Route::get('/member', function () {
-    return view('member.index');
-});
+// // M
+// Route::get('/member', function () {
+//     return view('member.index');
+// });
+
+// STAFF
 
 Route::get('/staff', function () {
     return view('staff.index');
 });
 
-// COMITE
-Route::prefix('committee')->prefix('committee')->group(function () {
-    Route::controller(EventController::class)->group(function () {
-        // Route::get('', 'index')->name('comite.index');
-    });
-
-    Route::controller(EventController::class)
-        ->prefix('events')
-        ->group(function () {
-            Route::get('', 'index')->name('event.index');
-            Route::get('add', 'add')->name('event.add');
-            Route::post('store', 'store')->name('events.store');
-            Route::get('edit/{id}', 'edit')->name('events.edit');
-            // Route::put('update/{id}', 'update')->name('events.update');
-            Route::get('delete/{id}', 'delete')->name('events.delete');
-        });
-});
