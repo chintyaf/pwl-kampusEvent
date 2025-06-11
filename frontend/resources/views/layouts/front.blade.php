01b5e6a73300991be89197c4bc08c270db9fc428
chintyaf <chintya.elysia@gmail.com>
2025-06-11 11:52:32 +0700

    Update navbar

--- frontend/resources/views/layouts/front.blade.php
+++ frontend/resources/views/layouts/front.blade.php
@@ -3,25 +3,26 @@
 
 <head>
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
-    rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
 
     <title>Evoria</title>
 
-  <!-- Bootstrap core CSS -->
-  <link href="{{ asset('front/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('front/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 
-  <!-- Additional CSS Files -->
-  <link rel="stylesheet" href="{{ asset('front/assets/css/fontawesome.css') }}">
-  <link rel="stylesheet" href="{{ asset('front/assets/css/templatemo-scholar.css') }}">
-  <link rel="stylesheet" href="{{ asset('front/assets/css/owl.css') }}">
-  <link rel="stylesheet" href="{{ asset('front/assets/css/animate.css') }}">
-  <link rel="stylesheet" href="{{ asset('front/assets/css/style.css') }}">
-  <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
-  <!--
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="{{ asset('front/assets/css/fontawesome.css') }}">
+    <link rel="stylesheet" href="{{ asset('front/assets/css/templatemo-scholar.css') }}">
+    <link rel="stylesheet" href="{{ asset('front/assets/css/owl.css') }}">
+    <link rel="stylesheet" href="{{ asset('front/assets/css/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('front/assets/css/style.css') }}">
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+    <!--
 
 TemplateMo 586 Scholar
 
@@ -32,20 +33,20 @@ https://templatemo.com/tm-586-scholar
 
 <body>
 
-  <!-- ***** Preloader Start ***** -->
-  <div id="js-preloader" class="js-preloader">
-    <div class="preloader-inner">
-      <span class="dot"></span>
-      <div class="dots">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
+    <!-- ***** Preloader Start ***** -->
+    <div id="js-preloader" class="js-preloader">
+        <div class="preloader-inner">
+            <span class="dot"></span>
+            <div class="dots">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
     </div>
-  </div>
-  <!-- ***** Preloader End *****-->
+    <!-- ***** Preloader End *****-->
 
-  @include('layouts.partials-front.navbar')
+    @include('layouts.partials-front.navbar')
 
 
 
@@ -57,12 +58,12 @@ https://templatemo.com/tm-586-scholar
 
 
 
-  @include('layouts.partials-front.footer')
+    @include('layouts.partials-front.footer')
 
 
-  <!-- Scripts -->
-  @yield('extraJS')
-  <!-- Bootstrap core JavaScript -->
+    <!-- Scripts -->
+    @yield('extraJS')
+    <!-- Bootstrap core JavaScript -->
     <script src="{{ asset('front/vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('front/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('front/assets/js/isotope.min.js') }}"></script>
@@ -70,6 +71,16 @@ https://templatemo.com/tm-586-scholar
     <script src="{{ asset('front/assets/js/counter.js') }}"></script>
     <script src="{{ asset('front/assets/js/custom.js') }}"></script>
 
+    <script>
+        $(".logout-btn").click(function(e) {
+            e.preventDefault(); // prevent default link behavior
+            const url = $(this).data("url");
+            const form = $("#logout-form");
+
+            form.attr("action", url);
+            form.submit();
+        });
+    </script>
 </body>
 
 </html>
--- frontend/resources/views/layouts/partials-front/navbar.blade.php
+++ frontend/resources/views/layouts/partials-front/navbar.blade.php
@@ -1,40 +1,47 @@
   <!-- ***** Header Area Start ***** -->
   <header class="header-area header-sticky background-header">
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <nav class="main-nav">
-            <!-- ***** Logo Start ***** -->
-            <a href="/" class="logo">
-              <h1>Evoria</h1>
-            </a>
-            <!-- ***** Logo End ***** -->
-            <!-- ***** Serach Start ***** -->
-            <div class="search-input">
-              <form id="search" action="#">
-                <input type="text" placeholder="Type Something" id='searchText' name="searchKeyword"
-                  onkeypress="handle" />
-                <i class="fa fa-search"></i>
-              </form>
-            </div>
-            <!-- ***** Serach Start ***** -->
-            <!-- ***** Menu Start ***** -->
-            <ul class="nav">
-              <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
-              <!-- <li class="scroll-to-section"><a href="#services">Services</a></li> -->
-              <!-- <li class="scroll-to-section"><a href="#courses">Courses</a></li> -->
-              <!-- <li class="scroll-to-section"><a href="#team">Team</a></li> -->
-              <li class="scroll-to-section"><a href="#events">Events</a></li>
-              <li class="scroll-to-section"><a href="{{ route('login') }}">Log in</a></li>
-              <li class="scroll-to-section"><a href="{{ route('register') }}">Register</a></li>
-            </ul>
-            <a class='menu-trigger'>
-              <span>Menu</span>
-            </a>
-            <!-- ***** Menu End ***** -->
-          </nav>
-        </div>
+      <div class="container">
+          <div class="row">
+              <div class="col-12">
+                  <nav class="main-nav">
+                      <!-- ***** Logo Start ***** -->
+                      <a href="/" class="logo">
+                          <h1>Evoria</h1>
+                      </a>
+                      <!-- ***** Logo End ***** -->
+                      <!-- ***** Serach Start ***** -->
+                      <div class="search-input">
+                          <form id="search" action="#">
+                              <input type="text" placeholder="Type Something" id='searchText' name="searchKeyword"
+                                  onkeypress="handle" />
+                              <i class="fa fa-search"></i>
+                          </form>
+                      </div>
+                      <!-- ***** Serach Start ***** -->
+                      <!-- ***** Menu Start ***** -->
+                      <ul class="nav">
+                          <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                          <!-- <li class="scroll-to-section"><a href="#services">Services</a></li> -->
+                          <!-- <li class="scroll-to-section"><a href="#courses">Courses</a></li> -->
+                          <!-- <li class="scroll-to-section"><a href="#team">Team</a></li> -->
+                          <li class="scroll-to-section"><a href="#events">Events</a></li>
+                          @if (!session()->has('token'))
+                              <li class="scroll-to-section"><a href="{{ route('login') }}">Log in</a></li>
+                              <li class="scroll-to-section"><a href="{{ route('register') }}">Register</a></li>
+                          @else
+                              <li class="scroll-to-section"><a data-url="/logout">Log Out</a></li>
+                              <form id="logout-form" method="POST" style="display: none;">
+                                  <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                              </form>
+                          @endif
+                      </ul>
+                      <a class='menu-trigger'>
+                          <span>Menu</span>
+                      </a>
+                      <!-- ***** Menu End ***** -->
+                  </nav>
+              </div>
+          </div>
       </div>
-    </div>
   </header>
   <!-- ***** Header Area End ***** -->
